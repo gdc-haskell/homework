d8527054b07b2d8d9f6b0aebbf67aef5cd059b34
ParkJuhyeong <majecty@gmail.com>
2015-01-07 21:04:36 +0900

    Add first spec4 test.

--- Spec4.hs
+++ Spec4.hs
@@ -1,10 +1,23 @@
 
 import Test.Hspec
-import Test.QuickCheck
-import Control.Exception (evaluate)
+-- import Test.QuickCheck
+-- import Control.Exception (evaluate)
+import BST
+import HW04
+
+instance Eq a => Eq (BST a) where
+  Leaf == Leaf = True
+  Node llTree lhsElement lrTree == Node rlTree rhsElement rrTree = (llTree == rlTree) && (lhsElement == rhsElement) && (lrTree == rrTree)
+  Leaf == Node _ _ _ = False
+  Node _ _ _ == Leaf = False
+
 
 main :: IO()
 main = hspec $ do
   describe "Prelude.heade" $ do
     it "returns the first element of a list" $ do
       head [23 ..] `shouldBe` (23 :: Int)
+
+  describe "Insert BST" $ do
+    it "empty tree insert success" $ do
+      insertBST (compare :: Int -> Int -> Ordering) 3 Leaf `shouldBe` Node Leaf 3 Leaf
