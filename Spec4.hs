40407697fc3313d0f98f39de42105c0e3fe29592
SeongChan Lee <foriequal@gmail.com>
2015-01-08 01:42:27 +0900

    Squashed 'tests/' changes from 3c8101e..1d485c2
    1d485c2 Add Mod5 tests.
    3ea2cfd Add asLists tests.
    3019a51 Add firstLetters tests.
    e52420b Add dropTrailingWhitespace tests.
    9a3deba Add allCaps tests.
    507c810 subtree test haha.
    d852705 Add first spec4 test.
    
    git-subtree-dir: tests
    git-subtree-split: 1d485c255383be55ba927d1aa6de6818a8f7e9ed

--- Spec4.hs
+++ Spec4.hs
@@ -1,10 +1,63 @@
 
 import Test.Hspec
-import Test.QuickCheck
-import Control.Exception (evaluate)
+-- import Test.QuickCheck
+-- import Control.Exception (evaluate)
+import BST
+import HW04
+
+instance Eq a => Eq (BST a) where
+  Leaf == Leaf = True
+  Node llTree lhsElement lrTree == Node rlTree rhsElement rrTree = (llTree == rlTree) && (lhsElement == rhsElement) && (lrTree == rrTree)
+  Leaf == Node _ _ _ = False
+  Node _ _ _ == Leaf = False
+
+compareInt :: Int -> Int -> Ordering
+compareInt = compare
 
 main :: IO()
 main = hspec $ do
   describe "Prelude.heade" $ do
     it "returns the first element of a list" $ do
       head [23 ..] `shouldBe` (23 :: Int)
+
+  describe "Insert BST" $ do
+    it "empty tree insert success" $ do
+      insertBST compareInt 3 Leaf `shouldBe` Node Leaf 3 Leaf
+    it "insert two value success" $ do
+      insertBST compareInt 5 (insertBST compareInt 3 Leaf) `shouldBe` Node Leaf 3 (Node Leaf 5 Leaf)
+
+  describe "allCaps" $ do
+    it "should success." $ do
+      allCaps ["Hi","There"] `shouldBe` True
+    it "should success when empty list" $ do
+      allCaps [] `shouldBe` True
+    it "should fail with empty string." $ do
+      allCaps ["", "Blah"] `shouldBe` False
+    it "should fail with fail input." $ do
+      allCaps ["Hi","there"] `shouldBe` False
+
+  describe "dropTrailingWhitespace" $ do
+    it "should return same which does not have trailing whitespace" $ do
+      dropTrailingWhitespace "foo" `shouldBe` "foo"
+    it "should can get empty string as input" $ do
+      dropTrailingWhitespace "" `shouldBe` ""
+    it "should remove trailing whitespace " $ do
+      dropTrailingWhitespace "bar  " `shouldBe` "bar"
+
+  describe "firstLetters" $ do
+    it "should return first letters" $ do
+      firstLetters [ "foo", "bar" ] `shouldBe` [ 'f', 'b' ]
+    it "should ignore empty string" $ do
+      firstLetters [ "foo", "" ] `shouldBe` [ 'f' ]
+    it "should return empty list when input empty" $ do
+      firstLetters [ ] `shouldBe` [ ]
+    it "should return empty list when input is all empty string" $ do
+      firstLetters [ "", "" ] `shouldBe` [ ]
+
+  describe "asList" $ do
+    it "should concat strings" $ do
+      asList ["alpha","beta","gamma"] `shouldBe` "[alpha,beta,gamma]"
+    it "should get empty list" $ do
+      asList [] `shouldBe` "[]"
+    it "should get one argument" $ do
+      asList ["lonely"] `shouldBe` "[lonely]"
